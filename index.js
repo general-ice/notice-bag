1219a9d3ef75aded08903e8fa83b5f70a495c8d9
asgeneral <v.mozdukov@dev.userstory.ru>
2018-12-25 22:14:48 +0700

    init

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const {app, BrowserWindow} = require('electron');
 
-function main() {
+const defaultWindowsProperties = {
+    height: 300,
+    width: 500,
+};
 
+const appWindows = {};
+
+function main() {
+    const {height, width} = defaultWindowsProperties;
+
+    appWindows.mainPage = new BrowserWindow({
+        height,
+        width
+    });
+
+    appWindows.mainPage.loadFile(`${__dirname}/views/mainPage.html`);
+
+    appWindows.mainPage.on('closed', () => {
+        appWindows.mainPage = null;
+    });
 }
+
+app.on('ready', main);
+
+app.on('window-all-closed', () => {
+    app.quit();
+})
